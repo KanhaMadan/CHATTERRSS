fcda6bd3279162ab51af552db64433f5c65ce1f4
Kanha Madan <121640438+KanhaMadan@users.noreply.github.com>
2024-05-08 19:18:12 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://localhost:5000", {
+            const socket = io("https://chat-8pbv.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
